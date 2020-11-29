3f49a6c29fd2d187391bc0c74145debc12550e85
Katsiaryna Pravasud <pravosud@gmail.com>
2020-11-29 23:13:33 +0300

    fix: add new bg

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 function removeTransition(e) {
-    if(e.propertyName !== 'transform') return;
+    if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
-}
+  }
 
-function playSound(e) {
+  function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return;
-    
+
+    key.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
-    key.classList.add('playing');
-}
+  }
 
-
-
- const keys = document.querySelectorAll('.key');
- keys.forEach(key => key.addEventListener('transitionend', removeTransition));
- window.addEventListener('keydown', playSound);
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
+  
